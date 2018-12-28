6877e396918a7d41b989c401b47827b7c4b860b2
DONS-PC\Priya Yudha Swandana <priyayudha.sw27@gmail.com>
2018-12-28 15:02:09 +0700

    uploadfile

--- application/controllers/uploadFile.php
+++ application/controllers/uploadFile.php
@@ -30,6 +30,9 @@ class uploadFile extends CI_Controller{
                 {       
                         $nama = $this->upload->data('file_name');
                         $this->cetakModel->upload($nama);
+                        $id = $this->cetakModel->lastID();
+                        $id_berkas = print_r($id['asd']);
+                        $this->session->set_userdata('id_berkas', $id_berkas);
                 		$this->session->set_userdata('namaFile', '<i class="fa fa-check" style="margin-left: 13px"> </i>'.$nama);
                         $this->session->set_userdata('status', '<i class="fa fa-check" style="margin-left: 13px"> </i> File berhasil di Upload');
                         redirect('pengaturanFile');
